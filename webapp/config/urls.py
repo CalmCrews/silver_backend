56b78aa4187e16ec4a5333a461d37e1ed9c23491
koomchang <leahpar0401@gmail.com>
2023-07-27 13:03:17 +0900

    feat: JWT를 이용한 로그아웃 (refresh token 삭제) 구현

--- webapp/config/urls.py
+++ webapp/config/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -9,12 +9,10 @@ router = routers.DefaultRouter()
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="silver-project",
+        title="moyeo",
         default_version='v1',
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
+        description="moyeo API 문서",
+        contact=openapi.Contact(email="leahpar0401@gmail.com"),
     ),
     public=True,
     permission_classes=[permissions.AllowAny],
@@ -22,10 +20,12 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('user.urls')),
+
 ]
 
 urlpatterns += [
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name="schema-json"),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name="schema-json"),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
