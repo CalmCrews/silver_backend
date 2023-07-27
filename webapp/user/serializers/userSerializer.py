16dcd2bc8c98e91f051208d103e4bbeb2f3c6bb0
koomchang <leahpar0401@gmail.com>
2023-07-27 13:02:13 +0900

    feat: JWT 를 이용한 회원가입 구현

---
+++ webapp/user/serializers/userSerializer.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+
+from user.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'username',
+            'profile_image',
+            'password',
+        ]
+
+        extra_kwargs = {
+            'password': {
+                'write_only': True
+            }
+        }
+
+    def create(self, validated_data):
+        username = validated_data['username']
+        password = validated_data['password']
+        user = User(
+            username=username,
+        )
+        user.set_password(password)
+        user.save()
+        return user
--- webapp/user/views/registerAPIView.py
+++ webapp/user/views/registerAPIView.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
-# Create your views here.
+from user.serializers import UserSerializer
+
+
+class RegisterAPIView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+
+            token = TokenObtainPairSerializer.get_token(user)
+            refresh_token = str(token)
+            access_token = str(token.access_token)
+            res = Response(
+                {
+                    "user": serializer.data,
+                    "message": "회원가입 성공",
+                    "token": {
+                        "access": access_token,
+                        "refresh": refresh_token,
+                    },
+                },
+                status=200,
+            )
+
+            return res
+        failed_res = {"message": "회원가입 실패"}
+        return Response(failed_res, status=400)
