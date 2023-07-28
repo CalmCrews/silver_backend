8b45cb9e097e6032ecfbea9275119fd03e782765
imjanghyeok <qkdrhkgn23@naver.com>
2023-07-28 22:33:16 +0900

    feat: club admin 구현

---
+++ webapp/club/admin/__init__.py
@@ -0,0 +1,1 @@
+from .clubAdmin import ClubAdmin
---
+++ webapp/club/admin/clubAdmin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from club.models import Club
+
+
+# Register your models here.
+@admin.register(Club)
+class ClubAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'code',
+        'intro',
+        'level',
+    ]
