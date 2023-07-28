fd0409638eb102211e7f6ccc56376b4eb3b9347a
koomchang <leahpar0401@gmail.com>
2023-07-28 23:13:48 +0900

    feat: datetime 포맷 변경

--- webapp/config/settings/base.py
+++ webapp/config/settings/base.py
@@ -137,6 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'user.User'
 
 REST_FRAMEWORK = {
+    'DATETIME_FORMAT': "%Y-%m-%d / %H:%M:%S",
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'config.authentications.CsrfExemptSessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
