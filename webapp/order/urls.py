0dc941bc212b1724082fae2c927771000e8fea98
koomchang <leahpar0401@gmail.com>
2023-07-30 02:56:19 +0900

    feat: order view 관련 Url 연결

--- webapp/order/urls.py
+++ webapp/order/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from order.views import OrderCreateAPIView
+from order.views import OrderListCreateAPIView, OrderRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
-    path("", OrderCreateAPIView.as_view()),
-
+    path("", OrderListCreateAPIView.as_view()),
+    path("<int:order_id>/", OrderRetrieveUpdateDestroyAPIView.as_view()),
 ]
