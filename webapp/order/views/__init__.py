5b1d947d1997cda36c03a54f346988bb6dbaf506
koomchang <leahpar0401@gmail.com>
2023-07-30 01:49:27 +0900

    feat: 주문 생성 시 상품 재고와 구매 가능 상태를 고려하여 생성 가능하도록 구현

---
+++ webapp/order/views/__init__.py
@@ -0,0 +1,1 @@
+from .orderCreateAPIView import OrderCreateAPIView
---
+++ webapp/order/views/orderCreateAPIView.py
@@ -0,0 +1,36 @@
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+
+from order.models import Order
+from order.serializers.orderSerializer import OrderSerializer
+from product.models import Product
+
+
+class OrderCreateAPIView(CreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def perform_create(self, serializer):
+        product = self.get_product()
+        serializer.save(
+            user=self.request.user,
+            product=product,
+            initial_price=product.price,
+            status='PREPAYMENT'
+        )
+        if serializer.instance.final_price is None:
+            serializer.instance.final_price = product.price
+            serializer.save()
+
+    def post(self, request, *args, **kwargs):
+        product = self.get_product()
+        if product.is_sellable:
+            if product.current_buyable_quantity < int(request.data.get('quantity')):
+                return Response({'message': '해당 상품의 재고가 부족합니다'}, status=400)
+            return super().post(request, *args, **kwargs)
+        return Response({'message': '해당 상품을 구매할 수 없습니다'}, status=400)
+
+    def get_product(self):
+        product_id = self.kwargs.get('product_id')
+        product = Product.objects.get(id=product_id)
+        return product
