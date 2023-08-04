718610cdbffe24e78633ebb07c182b0cc426b69a
imjanghyeok <qkdrhkgn23@naver.com>
2023-08-04 16:19:47 +0900

    refactor: clubRetrieveUpdate 로그인 유저 없을 시 메시지 전달

--- webapp/club/views/clubRetrieveUpdateAPIView.py
+++ webapp/club/views/clubRetrieveUpdateAPIView.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.generics import RetrieveUpdateAPIView
 from rest_framework.response import Response
 
@@ -10,7 +11,15 @@ class ClubRetrieveUpdateAPIView(RetrieveUpdateAPIView):
     serializer_class = ClubRetrieveSerializer
     lookup_url_kwarg = 'club_id'
 
+    def retrieve(self, request, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            return Response({'message': '로그인 후 이용해 주세요.'}, status=status.HTTP_400_BAD_REQUEST)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
     def update(self, request, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            return Response({'message': '로그인 후 이용해 주세요.'}, status=status.HTTP_400_BAD_REQUEST)
         instance = self.get_object()
         club = instance.club
 
