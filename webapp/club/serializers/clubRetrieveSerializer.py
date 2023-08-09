004c59bdd14cad4067815066386808c68d55a788
imjanghyeok <qkdrhkgn23@naver.com>
2023-08-09 21:45:20 +0900

    refactor: clubRetrieve API, Serializer 수정

--- webapp/club/serializers/clubRetrieveSerializer.py
+++ webapp/club/serializers/clubRetrieveSerializer.py
@@ -4,7 +4,6 @@ from club.models import Club
 
 
 class ClubRetrieveSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Club
         fields = (
@@ -17,4 +16,14 @@ class ClubRetrieveSerializer(serializers.ModelSerializer):
 
         read_only_fields = (
             'level',
+            'tag',
         )
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        tag_value = representation.get('tag', None)
+
+        if tag_value:
+            representation['tag'] = tag_value.split()
+
+        return representation
