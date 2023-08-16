b65fc26106f1c9f70010903d27452694550da05d
koomchang <leahpar0401@gmail.com>
2023-08-16 21:09:26 +0900

    fix: clubProductSerializer에 seller 정보 추가

--- webapp/club/serializers/clubProductSerializer.py
+++ webapp/club/serializers/clubProductSerializer.py
@@ -3,17 +3,20 @@ from rest_framework import serializers
 from club.models import ClubProduct
 from club.serializers import ClubSerializer
 from product.serializers import ProductAbstractSerializer
+from seller.serializers import SellerSerializer
 
 
 class ClubProductSerializer(serializers.ModelSerializer):
     product = ProductAbstractSerializer(read_only=True)
     club = ClubSerializer(read_only=True)
+    seller = serializers.SerializerMethodField()
 
     class Meta:
         model = ClubProduct
         fields = (
             'id',
             'product',
+            'seller',
             'club',
             'participant_count',
             'quantity_sum',
@@ -24,9 +27,14 @@ class ClubProductSerializer(serializers.ModelSerializer):
         read_only_fields = (
             'id',
             'product',
+            'seller',
             'participant_count',
             'quantity_sum',
             'current_price',
             'discount_rate',
             'achievement_rate',
         )
+
+    def get_seller(self, obj):
+        seller = obj.product.seller
+        return SellerSerializer(seller).data
