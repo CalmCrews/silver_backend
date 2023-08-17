fa7c1ddd6a908149666568fc15a504a51e3e7631
imjanghyeok <qkdrhkgn23@naver.com>
2023-08-17 10:40:03 +0900

    refactor: product에 각종 정보 추가, conflict resolve

--- webapp/notification/serializers/usernotificationRetrieveSerializer.py
+++ webapp/notification/serializers/usernotificationRetrieveSerializer.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.generics import get_object_or_404
 
 from club.models import ClubProduct
 from club.serializers import ClubProductSerializer
@@ -8,7 +9,7 @@ from notification.serializers import NotificationSerializer
 
 class UserNotificationRetrieveSerializer(serializers.ModelSerializer):
     notification = NotificationSerializer()
-    clubproduct = serializers.SerializerMethodField()
+    club_product = serializers.SerializerMethodField()
 
     class Meta:
         model = UserNotification
@@ -16,12 +17,25 @@ class UserNotificationRetrieveSerializer(serializers.ModelSerializer):
             'notification',
             'receiver',
             'is_read',
-            'clubproduct',
+            'club_product',
         )
 
-    def get_clubproduct(self, obj):
+    def get_club_product(self, obj):
         club_product_id = obj.notification.clubproduct
-        print(club_product_id)
-        club_product = ClubProduct.objects.get(id=club_product_id)
-        print(club_product)
-        return ClubProductSerializer(club_product).data
+        club_product = get_object_or_404(ClubProduct, id=club_product_id)
+        serializer = ClubProductSerializer(club_product).data
+        participant_count = club_product.participant_count
+        quantity_sum = club_product.quantity_sum
+        current_price = club_product.current_price
+        discount_rate = club_product.discount_rate
+        achievement_rate = club_product.achievement_rate
+
+        extra_data = {
+            'participant_count': participant_count,
+            'quantity_sum': quantity_sum,
+            'current_price': current_price,
+            'discount_rate': discount_rate,
+            'achievement_rate': achievement_rate,
+        }
+        serializer['product'].update(extra_data)
+        return serializer
