c59e88e41bd94eabf492177ea3b84ff1102c6bed
koomchang <leahpar0401@gmail.com>
2023-08-17 15:57:55 +0900

    fix: 모임에 대한 상품 url 변경

--- webapp/club/urls.py
+++ webapp/club/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("", ClubListCreateAPIView.as_view()),
     path("<int:club_id>", ClubRetrieveUpdateAPIView.as_view()),
     path("<int:club_id>/clubProducts", ClubProductListAPIView.as_view()),
-    path("<int:club_id>/clubProducts/<int:club_product_id>", ClubProductRetrieveAPIView.as_view()),
+    path("clubProducts/<int:club_product_id>", ClubProductRetrieveAPIView.as_view()),
     path("join/", UserClubJoinCreateAPIView.as_view()),
 ]
