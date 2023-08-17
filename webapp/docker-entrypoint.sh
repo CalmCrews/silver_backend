7ccd3447b955fb2653f600d1e093d42bf7135e12
imjanghyeok <qkdrhkgn23@naver.com>
2023-08-18 03:24:36 +0900

    feat: intial data 설정

--- webapp/docker-entrypoint.sh
+++ webapp/docker-entrypoint.sh
@@ -2,6 +2,10 @@
 
 python manage.py migrate --noinput
 
+python manage.py loaddata seller.json
+
+python manage.py loaddata product.json
+
 python manage.py collectstatic --noinput
 
 python manage.py runserver 0.0.0.0:8888
