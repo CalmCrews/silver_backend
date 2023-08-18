43e53826ae36abba5d60a7da02fa926593d513dd
imjanghyeok <qkdrhkgn23@naver.com>
2023-08-18 20:53:15 +0900

    fix: allclubproduct Filtering 내가 구매한 상품 제거

--- webapp/club/views/allClubProductListAPIView.py
+++ webapp/club/views/allClubProductListAPIView.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 
 from club.models import ClubProduct, UserClub
 from club.serializers import ClubProductAbstractSerializer
+from order.models import Order
 
 
 class AllClubProductListAPIView(ListAPIView):
@@ -14,7 +15,11 @@ class AllClubProductListAPIView(ListAPIView):
     def get_queryset(self):
         user_clubs = UserClub.objects.filter(user=self.request.user)
         club_ids = [user_club.club.pk for user_club in user_clubs]
-        queryset = self.queryset.filter(club__id__in=club_ids).order_by('product__end_at')
+        queryset = self.queryset.filter(club__id__in=club_ids)
+        club_product_id = [query.id for query in queryset]
+        already_buy = Order.objects.filter(user=self.request.user, product__in=club_product_id)
+        already_buy_ids = [buy.product.id for buy in already_buy]
+        queryset = queryset.exclude(id__in=already_buy_ids)
         return queryset
 
     def list(self, request, *args, **kwargs):
