16ec1769dcdb40e0e02c6e09c58cdd52b40b785a
imjanghyeok <qkdrhkgn23@naver.com>
2023-08-18 23:21:32 +0900

    feature: docker 명령어

--- webapp/docker-entrypoint.sh
+++ webapp/docker-entrypoint.sh
@@ -6,6 +6,18 @@ python manage.py loaddata seller.json
 
 python manage.py loaddata product.json
 
+python manage.py loaddata user.json
+
+python manage.py loaddata club.json
+
+python manage.py loaddata userclub.json
+
+python manage.py loaddata clubproduct.json
+
+python manage.py loaddata order.json
+
+python manage.py loaddata review.json
+
 python manage.py collectstatic --noinput
 
 python manage.py runserver 0.0.0.0:8888
